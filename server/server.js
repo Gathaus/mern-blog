fa0ded4675c870fea735161c240505b61a4bc153
gathaus <rizamertyagci@gmail.com>
2020-09-29 04:23:07 +0300

    heroku connection

--- server/server.js
+++ server/server.js
@@ -19,8 +19,9 @@ const PORT = process.env.PORT;
 app.use(cors());
 
 app.use("/api", routes);
+
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/build"));
   });
 
 app.use(express.static("client/build"));
