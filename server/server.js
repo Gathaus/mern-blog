f100c1d74db9ee9891ff8a9688b8ecb959f98478
gathaus <rizamertyagci@gmail.com>
2020-09-29 04:32:10 +0300

    heroku connection

--- server/server.js
+++ server/server.js
@@ -20,11 +20,13 @@ app.use(cors());
 
 app.use("/api", routes);
 
+
+app.use(express.static("client/build"));
+
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build","index.html"));
   });
 
-app.use(express.static("client/build"));
 
 app.listen(PORT || 5000, () => {
 	console.log(`App started on ${PORT} `);
