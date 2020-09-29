4de33e0baafb991a8e1dc75738ffcd0cc19ab7a2
gathaus <rizamertyagci@gmail.com>
2020-09-29 04:24:10 +0300

    heroku connection

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use("/api", routes);
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build"));
+    res.sendFile(path.resolve(__dirname, "../client/build","index.html"));
   });
 
 app.use(express.static("client/build"));
